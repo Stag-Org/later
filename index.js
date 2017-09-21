077516893ff9416cf49463d0c156c8715fc147d2
Nick Gault <ngault@users.noreply.github.com>
2017-09-21 08:58:32 -0700

    change for Webpack compat., the 1 reason for cloning this repo

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ globals.forEach(function(g) {
   if (g in global) globalValues[g] = global[g];
 });
 
-require(process.env['LATER_COV'] ? "./later-cov" : "./later");
+// NOTE: change by Nick Gault for Webpack compatibility
+// require(process.env['LATER_COV'] ? "./later-cov" : "./later");
+require("./later");
 
 module.exports = later;
 
